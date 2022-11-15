0a1fda607a973aa34476d709033ed0d860a67338
Nguyen Van Hieu <vanhieu97kt@gmail.com>
2022-11-15 23:41:58 +0700

    bt

---
+++ main.js
@@ -0,0 +1,58 @@
+// gọi biến sum =2; num = 2() => vì num là số nguyên dương nên num > 0  => sum = 1
+// dùng vòng lặp while với điều kiện sum < 10000
+// num sẽ cộng dồng lên 1
+// sum = sum + num đến khi hàm đủ điện kiện
+function min() {
+  var sum = 1;
+  var num = 1;
+  while (sum < 10000) {
+    num++;
+    sum = sum + num;
+  }
+  document.getElementById("min").innerHTML = "Kết quả là " + num;
+}
+// gọi biến x
+// gọi biến n
+// gán biến sum
+// dùng for (var i = 1; i <= n; i++ ){
+// sum += Math.pow(x, i)
+// }
+function sum() {
+  var x = document.getElementById("num_x").value * 1;
+  var n = document.getElementById("num_n").value * 1;
+  var sum = 0;
+  for (var i = 1; i <= n; i++) {
+    sum += Math.pow(x, i);
+  }
+  document.getElementById("sum").innerHTML = "Kết quả là " + sum;
+}
+
+// gọi biên n
+// gán  biến tich
+// dùng for(i = 1; i <= n, i++){
+//     tich = n**
+// }
+function tich() {
+  var n = document.getElementById("n").value * 1;
+  var tich = 1;
+  for (var i = 1; i <= n; i++) {
+    tich *= i;
+  }
+  document.getElementById("tinh").innerHTML = "Kết quả là " + tich;
+}
+
+function div(){
+    var divChan = '<div style=" background-color: red; width: 500px; height: 30px;">div chẵn</div> ';
+    var divLe = '<div style=" background-color: blue; width: 500px; height: 30px;">div lẻ</div>';
+    var content = '';
+    for(var i = 1; i <= 10; i++){
+        if((i + 1) % 2 === 0){
+            content += divChan  + '<br/>'
+        }else{
+            content += divLe + '<br/>'
+        }
+    }
+    document.getElementById("hienthi").innerHTML = content
+}
+
+
